bb7b287deecf316c11d4b3c54161c59c0b54d684
Tonyy456 <ajdalesandro0115@gmail.com>
2023-05-10 15:42:52 -0400

    designed a ui and created basic files

---
+++ projects/4_meshAndCube/src/MainQTWindow.h
@@ -0,0 +1,13 @@
+#ifndef MT_MAIN_WINDOW_H
+#define MT_MAIN_WINDOW_H
+
+class MainQTWindow
+{
+    public:
+        MainQTWindow();
+        ~MainQTWindow();
+    private:
+        int foo;
+}
+
+#endif
